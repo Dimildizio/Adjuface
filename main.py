b650fb112c15cb2ec709af4db199a3f64301ea54
Dimildizio <42382713+Dimildizio@users.noreply.github.com>
2023-12-27 07:56:33 +0800

    feat(bot): add main.py to run the bot

--- main.py
+++ main.py
@@ -5,5 +5,21 @@ from aiogram import Bot, Dispatcher
 from bot.message_handler import setup_handlers
 
 
+async def main(dp, iobot):
+    setup_handlers(dp)
+    await dp.start_polling(iobot)
+
+
+def get_token():
+    with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+    return config['token']
+
+
 if __name__ == '__main__':
-    print('main file')
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    ibot = Bot(token=get_token())
+    dispatcher = Dispatcher()
+    asyncio.run(main(dispatcher, ibot))
+
